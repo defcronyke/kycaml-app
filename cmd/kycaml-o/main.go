f5c811ae26e29aea5aac29b5a0c817ff7a3a913d
Jeremy Carter <jeremy@jeremycarter.ca>
2021-06-10 07:35:16 -0400

    Add cmd/kycaml-o utility.

---
+++ cmd/kycaml-o/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/defcronyke/kycaml"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Printf("Usage: %v \"<str1>\" \"<str2>\"\n", os.Args[0])
+		os.Exit(1)
+		return
+	}
+
+	args := os.Args[1:]
+
+	fmt.Printf("%v\n", kycaml.NewOSA(args[0], args[1]))
+}
