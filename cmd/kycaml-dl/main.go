5efc6dd8fec0b2960366e7312aa73524aa8fefd5
Jeremy Carter <jeremy@jeremycarter.ca>
2021-06-10 06:33:27 -0400

    Add cmd/kycaml-dl utility.

---
+++ cmd/kycaml-dl/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/defcronyke/kycaml"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Printf("Usage: %v \"<str1>\" \"<str2>\"\n", os.Args[0])
+		os.Exit(1)
+		return
+	}
+
+	args := os.Args[1:]
+
+	res := kycaml.NewDamerauLevenshtein(args[0], args[1])
+
+	fmt.Printf("%v\n", res)
+}
