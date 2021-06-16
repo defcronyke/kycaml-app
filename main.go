56fc1941a7eddddc9c26584acc02b8c5871bd357
Jeremy Carter <jeremy@jeremycarter.ca>
2021-06-15 21:31:09 -0400

    Add route documentation in code comments.

--- main.go
+++ main.go
@@ -53,19 +53,102 @@ func main() {
 
 	r := mux.NewRouter()
 
+	/** USA Sanctions-based Routes (Default) */
+
+	/** HTTP Route: GET /usa/names
+	Get the names of sanctioned entities.
+	(USA Sanctions-based) */
 	r.HandleFunc("/usa/names", k.NamesHandler)
+
+	/** HTTP Route: GET /usa/names-dm
+	Get the names of sanctioned entities, with a
+	double-metaphone encoded lookup key.
+	(USA Sanctions-based) */
+	r.HandleFunc("/usa/names-dm", k.NamesDMHandler)
+
+	/** HTTP Route: GET /usa/names-db
+	Get the names of sanctioned entities, with a
+	combination of all supported encodings of lookup
+	keys. (USA Sanctions-based) */
+	r.HandleFunc("/usa/names-db", k.NamesDBHandler)
+
+	/** HTTP Route: GET /usa/sdn
+	Get the USA's Specially Designated Nationals
+	And Blocked Persons list data, in a custom
+	unofficial JSON format.
+	Built from the official XML list found here:
+	https://home.treasury.gov/policy-issues/financial-sanctions/specially-designated-nationals-list-data-formats-data-schemas
+	-> SDN_ADVANCED.XML
+	(USA Sanctions-based) */
 	r.HandleFunc("/usa/sdn", k.USASdnJSONHandler)
+
+	/** HTTP Route: GET /usa/cons
+	Get the USA's Consolidated Advanced Sanctions
+	list data, in a custom unofficial JSON format.
+	Built from the official XML list found here:
+	https://home.treasury.gov/policy-issues/financial-sanctions/consolidated-sanctions-list-non-sdn-lists
+	-> CONS_ADVANCED.XML
+	(USA Sanctions-based) */
 	r.HandleFunc("/usa/cons", k.USAConsJSONHandler)
-	r.HandleFunc("/usa/score", k.ScoreHandler)
 
+	/* END: USA Sanctions-based Routes (default) */
+
+	/** Default Routes */
+
+	/** HTTP Route: GET /names
+	Get the names of sanctioned entities.
+	(USA Sanctions-based) */
 	r.HandleFunc("/names", k.NamesHandler)
-	r.HandleFunc("/sdn", k.USASdnJSONHandler)
-	r.HandleFunc("/cons", k.USAConsJSONHandler)
-	r.HandleFunc("/score", k.ScoreHandler)
 
+	/** HTTP Route: GET /names-dm
+	Get the names of sanctioned entities, with a
+	double-metaphone encoded lookup key.
+	(USA Sanctions-based) */
+	r.HandleFunc("/names-dm", k.NamesDMHandler)
+
+	/** HTTP Route: GET /names-db
+	Get the names of sanctioned entities, with a
+	combination of all supported encodings of lookup
+	keys. (USA Sanctions-based) */
+	r.HandleFunc("/names-db", k.NamesDBHandler)
+
+	/** HTTP Route: GET /sdn
+	Get the USA's Specially Designated Nationals
+	And Blocked Persons list data, in a custom
+	unofficial JSON format.
+	Built from the official XML list found here:
+	https://home.treasury.gov/policy-issues/financial-sanctions/specially-designated-nationals-list-data-formats-data-schemas
+	-> SDN_ADVANCED.XML
+	(USA Sanctions-based) */
+	r.HandleFunc("/sdn", k.USASdnJSONHandler)
+
+	/** HTTP Route: GET /cons
+	Get the USA's Consolidated Advanced Sanctions
+	list data, in a custom unofficial JSON format.
+	Built from the official XML list found here:
+	https://home.treasury.gov/policy-issues/financial-sanctions/consolidated-sanctions-list-non-sdn-lists
+	-> CONS_ADVANCED.XML
+	(USA Sanctions-based) */
+	r.HandleFunc("/cons", k.USAConsJSONHandler)
+
+	/* END: Default Routes */
+
+	/** Static File Routes */
+
+	/** HTTP Route: GET /usa/
+	Static file server which serves the contents of
+	the directory: `static/` */
 	r.PathPrefix("/usa/").Handler(http.StripPrefix("/usa/", http.FileServer(http.Dir(staticDir))))
+
+	/** HTTP Route: GET /
+	Static file server which serves the contents of
+	the directory: `static/` */
 	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir(staticDir))))
 
+	/* END: Static File Routes */
+
+	/** Attach the HTTP router/mux middleware to the
+	HTTP server. */
 	http.Handle("/", r)
 
 	srv := &http.Server{
